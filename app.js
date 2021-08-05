0e582fe6d7797784ba6facd2767a14fea196042d
Eshita Kalsi <eshitakalsi@Eshitas-MacBook-Pro.local>
2021-08-05 13:03:50 +0530

    Add Swagger Docs

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const cors = require("cors");
 const xss = require("xss-clean"); //cross side scripting
 const rateLimiter = require("express-rate-limit");
 
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+
 const express = require("express");
 const app = express();
 
@@ -27,11 +33,14 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.use(rateLimiter({ windowMs: 60 * 1000, max: 60 }));
-// extra packages
 
-app.get('/', (req,res)=>{
-  res.send('Watch-A-Job-API')
-})
+
+app.get("/", (req, res) => {
+  res.send('<h1>Watch-A-Job</h1><a href="/api-docs">Documentation</a>');
+});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
